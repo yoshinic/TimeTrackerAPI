c66e066d3bf82f02506e9cce227f8b00b045b219
yoshi <s.y.nakada@gmail.com>
2023-08-27 02:43:47 +0900

    修正：APIにpublic

--- Sources/TimeTrackerAPI/Factories/DefaultServiceFactory.swift
+++ Sources/TimeTrackerAPI/Factories/DefaultServiceFactory.swift
@@ -1,13 +1,12 @@
 import Fluent
 
-class DefaultServiceFactory {
-    static let shared = DefaultServiceFactory()
+public class DefaultServiceFactory {
+    public static let shared = DefaultServiceFactory()
 
     private let database: Database
 
-    lazy var activity: ActivityService = .init(db: database)
-
-    lazy var record: RecordService = .init(db: database)
+    public lazy var activity: ActivityService = .init(db: database)
+    public lazy var record: RecordService = .init(db: database)
 
     private init() {
         self.database = DatabaseManager().database
